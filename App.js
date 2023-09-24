b58d7b7e7a7e08e17f3571afb8ab61060d05384a
Avinabh Kumar <kumar.avi3112@gmail.com>
2023-09-24 16:44:10 +0530

    chapter-3

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//React.createElement =>js  object => HTML ELEMENT(render)
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-]);
+// JSX(transpiled before it reaches the js engine)=> parcel takes help from babel package to transpile)
+// JSX>>babel transpile>>  React.createElement =>js  object => HTML ELEMENT(render)
 
-console.log(parent);
+const title = (<h1 id="heading"> Namaste React using jsx</h1>);
+
+ 
+// React component
+// class based componentn - old way
+// functional component >> a component which return jsx
+
+// component composition
+const num=10000
+const HeadingComponent = () => (
+  <div>
+    <h2>{num+2}</h2>
+    {title}
+    <h1> namaste react function component</h1>
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+root.render(<HeadingComponent />);

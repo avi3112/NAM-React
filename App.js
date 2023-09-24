e7b62a8535fb5cadac313c5fdf51ff3e316f040d
Avinabh Kumar <kumar.avi3112@gmail.com>
2023-09-24 14:22:16 +0530

    chapter 2

--- App.js
+++ App.js
@@ -1,23 +1,10 @@
-/**
- *
- * <div id="parent">
- *      <div id="child">
- *          <h1>i am h1 tag</h1>
- *          <h2>i am h2 tag</h2>
- *      </div>
- *       <div id="child2">
- *          <h1>i am h1 tag</h1>
- *          <h2>i am h2 tag</h2>
- *      </div>
- * </div>
- *
- *
- * ReactElement(object)=> HTML (browser understand)
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h1", {}, "I am h tag"),
     React.createElement("h2", {}, "I am h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
@@ -25,8 +12,7 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h2", {}, "I am h2 tag"),
   ]),
 ]);
-// this is core react and very complicated
-// JSX bcz its very complicated
+
 console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
---
+++ README.md
@@ -0,0 +1,18 @@
+# Namaste React
+
+# Parcel 
+-Dev Build
+-Local Server
+-HMR = Hot module Replacement
+-File watching Algorithm - wrriten in c++
+-Caching -Faster Builds
+-Image optimization
+-Minification
+-Bundling
+-Compress
+-Code Splitting
+-Consistent Hashing
+-Differential Bundling
+-Error handling
+-Diagnostic
+-Tree Shaking = remove unused code
